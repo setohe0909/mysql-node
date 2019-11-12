3ac7f58c3fdb6936187b4fcad7bd8c63cbd23da1
sebastian tobon <setohe.09@gmail.com>
2019-11-11 22:10:18 -0500

    [DASHBOARD] List total of clients in the app

--- client/components/dashboard/Dashboard.js
+++ client/components/dashboard/Dashboard.js
@@ -1,42 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { CLIENTS } from '../../constants/entity';
+import * as crudAction from '../../actions/crudAction';
+
 import { pink, orange } from '@material-ui/core/colors';
 import { Grid } from '@material-ui/core';
 import { Business, Face } from '@material-ui/icons';
 
 import SummaryBox from './SummaryBox';
-import Product from './Product';
 
-const data = {
-  recentProducts: [
-    { id: 1, title: 'Samsung TV', text: 'Samsung 32 1080p 60Hz LED Smart HDTV.' },
-    { id: 2, title: 'Playstation 4', text: 'PlayStation 3 500 GB System' },
-    { id: 3, title: 'Apple iPhone 6', text: 'Apple iPhone 6 Plus 16GB Factory Unlocked GSM 4G ' },
-    { id: 4, title: 'Apple MacBook', text: 'Apple MacBook Pro MD101LL/A 13.3-Inch Laptop' }
-  ]
-};
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.actions.fetchAll(CLIENTS);
+  }
 
-const Dashboard = () => {
-  return (
-    <div>
-      <h2 style={{ paddingBottom: '15px' }}>Dashboard</h2>
+  render() {
+    const { clients } = this.props;
 
-      <Grid container spacing={4} style={{ marginBottom: '15px' }}>
-        <Grid item lg={3} sm={6} xl={3} xs={12}>
-          <SummaryBox Icon={Business} color={pink[600]} title="Total Clients" value="1500k" />
-        </Grid>
+    return (
+      <div>
+        <h1 style={{ paddingBottom: '15px' }}>Dashboard</h1>
+        {clients && (
+          <Grid container spacing={4} style={{ marginBottom: '15px' }}>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <SummaryBox Icon={Business} color={pink[600]} title="Total Clients" value={clients.data.length} />
+            </Grid>
+          </Grid>
+        )}
+      </div>
+    );
+  }
+}
 
-        <Grid item lg={3} sm={6} xl={3} xs={12}>
-          <SummaryBox Icon={Face} color={orange[600]} title="Total users" value="248" />
-        </Grid>
-      </Grid>
+/**
+ * Map the actions to props.
+ */
+const mapStateToProps = (state) => ({
+  clients: state.crud.client
+});
 
-      <Grid container spacing={24} style={{ marginBottom: '15px' }}>
-        <Grid item xs>
-          <Product data={data.recentProducts} />
-        </Grid>
-      </Grid>
-    </div>
-  );
-};
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(Object.assign({}, crudAction), dispatch)
+});
 
-export default Dashboard;
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
